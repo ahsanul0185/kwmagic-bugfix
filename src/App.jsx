8cd0cd9dea59c00e5e2b19a7c53b034bbd168555
Ahsanul Haque <ahsanul0185@gmail.com>
2025-03-23 18:57:02 +0600

    fixed android scroll

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,10 @@ function App() {
     AOS.refresh();
   }, []);
 
+  if (/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream) {
+    document.body.classList.add('ios-device');
+  }
+
   return (
     <AuthProvider>
       <BrowserRouter>
